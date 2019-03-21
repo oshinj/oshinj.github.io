181b9201b1ddc5cfeb50226cb10dee647489f592
oshinj <45084634+oshinj@users.noreply.github.com>
2019-03-20 19:18:31 -0700

    first edits

--- mysite/routes.py
+++ mysite/routes.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template
 from mysite import app
 
 @app.route("/")
-def index() :
+def home() :
     return render_template('home.html')
+
+@app.route("/projects/")
+def projects() :
+    return render_template('projects.html')
+
+@app.route("/about/")
+def about() :
+    return render_template('about.html')
 
 # @app.route("/hello<string:name>/")
 # def hello(name) :
